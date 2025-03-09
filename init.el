cfb42dbd3f7821264cf1d999bb6074d440b05546
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2025-03-09 16:21:28 -0400

    basic emacs config for java

---
+++ init.el
@@ -0,0 +1,16 @@
+;; emacs_java
+;;
+;; Create a variable to indicate where emacs's configuration is installed
+(setq EMACS_DIR "~/.emacs.d/")
+
+;; Avoid garbage collection at statup
+(setq gc-cons-threshold most-positive-fixnum ; 2^61 bytes
+      gc-cons-percentage 0.6)
+
+;; All the settings and package installation is set in configuration.org
+(org-babel-load-file "~/.emacs.d/emacs-configuration.org")
+
+(add-hook 'emacs-startup-hook
+  (lambda ()
+    (setq gc-cons-threshold 300000000 ; 300mb	
+          gc-cons-percentage 0.1)))
